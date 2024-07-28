1ad736d276455758abfe92786df1fd9d01e66925
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2024-07-28 22:40:43 +0300

    Update in how we create the orders table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -26,7 +26,7 @@ CREATE TABLE Customers(
 
 CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
-    customer_id INT FOREIGN KEY REFERENCES Customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
