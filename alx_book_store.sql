63cc4320e5be036954b17bbf6cb76e6f98c6249d
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2024-07-28 22:43:02 +0300

    Update in creation og order details table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,6 +33,7 @@ CREATE TABLE Orders(
 
 CREATE TABLE Order_Details(
     orderdetailid INT PRIMARY KEY,
-    order_id INT FOREIGN KEY REFERENCES Orders(order_id),
-    book_id INT FOREIGN KEY REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
 );
